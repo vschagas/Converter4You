bce53613d47e018ec0096251b4b7cf34016b69cb
valmir <valmirchagas88@gmail.com>
2023-01-16 18:22:03 -0300

    feat: add IResponse interface

---
+++ src/interfaces/IResponse.ts
@@ -0,0 +1,15 @@
+export interface IResponse {
+  ETHBRL: {
+    code: string,
+    codein: string,
+    name: string,
+    high: number
+    low: number,
+    varBid: number,
+    pctChange: number,
+    bid: number,
+    ask: number,
+    timestamp: string,
+    create_date: string
+  }
+}
